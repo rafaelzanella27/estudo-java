c169216bd106eecd82978c85a9e64b1012536937
razrodrigues <rafael.rodrigues@klab.com.br>
2022-04-13 11:56:41 -0300

    Configurando projeto

---
+++ README.md
@@ -0,0 +1,1 @@
+#Projeto para estudos e aplicação de exemplos dos conceitos básicos da linguagem JAVA.
---
+++ src/test/java/com/estudo/exemplos/ExemplosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.exemplos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemplosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
