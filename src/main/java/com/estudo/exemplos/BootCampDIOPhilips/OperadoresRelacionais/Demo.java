287e3780b542b348247261cc72f43f173816522b
razrodrigues <rafael.rodrigues@klab.com.br>
2022-04-28 08:36:44 -0300

    operadores logicos e relacionais

---
+++ src/main/java/com/estudo/exemplos/BootCampDIOPhilips/OperadoresRelacionais/Demo.java
@@ -0,0 +1,44 @@
+package com.estudo.exemplos.BootCampDIOPhilips.OperadoresRelacionais;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        int i1 = 10;
+        int i2 = 20;
+        float f1 = 4.5f;
+        float f2 = 3.5f;
+        double d1 = 59.6d;
+        char c1 = 'x';
+        char c2 = 'y';
+        String s1 = "Fulano";
+        String s2 = "Fulano";
+        String s3 = "Cicrano";
+        boolean b1 = true;
+        boolean b2 = false;
+
+        //operadores de similaridade
+        System.out.println("i1 == i2 " + (i1 == i2));
+        System.out.println("i1 != i2 " + (i1 != i2));
+
+        //operadores de tamanho
+        System.out.println("i1 > i2 " + (i1 > i2));
+        System.out.println("i1 <= i2 " + (i1 <= i2));
+
+        System.out.println("f1 == f2 " + (f1 == f2));
+        System.out.println("f1 != f2 " + (f1 != f2));
+        System.out.println("f1 >= f2 " + (f1 >= f2));
+        System.out.println("f1 < f2 " + (f1 < f2));
+
+        System.out.println("c1 == c2 " + (c1 == c2));
+        System.out.println("c1 != c2 " + (c1 != c2));
+        System.out.println("c1 > c2 " + (c1 > c2));
+        System.out.println("c1 <= c2 " + (c1 <= c2));
+
+        System.out.println("s1 == s2 " + (s1 == s2));
+        System.out.println("s1 == s3 " + (s1 == s3));
+        System.out.println("s1 != s2 " + (s1 != s2));
+
+        System.out.println("b1 == b2 " + (b1 == b2));
+        System.out.println("b1 != b2 " + (b1 != b2));
+    }
+}
